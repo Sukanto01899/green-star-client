88c9760df6ab5e14fd9ee1254e58114e1e281ef5
Sukanto Das <Suknto01899>
2023-05-18 18:46:16 +0600

    added

--- src/pages/UserDashboard.js/UserDashboardPage/Payment/CheckoutForm.js
+++ src/pages/UserDashboard.js/UserDashboardPage/Payment/CheckoutForm.js
@@ -18,7 +18,6 @@ const CheckoutForm = ({order}) => {
     const [paymentLoading, setPaymentLoading] = useState(false)
 
     useEffect(()=>{
-        setPaymentLoading(true)
         axios.post(`https://green-star.onrender.com/create-payment-intent`, {price}, {
             headers: {
                 'Authorization': `Bearer ${localStorage.getItem('access-token')}`
@@ -27,12 +26,10 @@ const CheckoutForm = ({order}) => {
         .then(res => {
             if(res.data.clientSecret){
                 setClientSecret(res.data.clientSecret)
-                setPaymentLoading(false)
             }
         })
         .catch(err => {
             toast.error(err.message)
-            setPaymentLoading(false)
         })
     }, [price])
 
